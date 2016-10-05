ba763cfdd40792f4492e45942cf17ca4541eb133
QualiSoftware <empresa2016sl@gmail.com>
2016-10-05 12:41:20 +0200

    damian el pollas

--- src/java/Acciones/HomeRopa.java
+++ src/java/Acciones/HomeRopa.java
@@ -5,12 +5,14 @@
  */
 package Acciones;
 
+import Modelos.Categoria;
 import Modelos.Clientela;
 import Modelos.Coleccion;
 import Modelos.Color;
 import Modelos.Fotos;
 import Modelos.Look;
 import Modelos.Marcas;
+import Modelos.Material;
 import Modelos.Ropa;
 import Modelos.RopaCategoria;
 import Modelos.RopaMaterial;
@@ -21,6 +23,7 @@ import com.opensymphony.xwork2.ActionSupport;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.apache.struts2.interceptor.validation.SkipValidation;
@@ -38,9 +41,19 @@ public class HomeRopa extends ActionSupport {
     private String titulo;
     private String boton;
     private Map sesion;
+    private String cabeceraocul;
+    private String botonocul;
     
     //variables específicas a cada controlador
     private ArrayList<Ropa> lista_ropa;
+    private List<Categoria> lista_categoria;
+    private List<Marcas> lista_marca;
+    private List<Clientela> lista_clientela;
+    private List<Look> lista_look;
+    private List<Tallas> lista_tallas;
+    private List<Material> lista_materiales;
+    private List<Color> lista_colores;
+    private List<Coleccion> lista_colecciones;
     
     
     private Integer roId2;
@@ -61,7 +74,176 @@ public class HomeRopa extends ActionSupport {
      private Set<RopaCategoria> ropaCategorias2 = new HashSet<RopaCategoria>(0);
      private Set<RopaMaterial> ropaMaterials2 = new HashSet<RopaMaterial>(0);
      private Set<Fotos> fotoses2 = new HashSet<Fotos>(0);
+     
+     //lista de combos
+     private List<String> lista_combo_categorias;
+     private List<String> lista_combo_marcas;
+     private List<String> lista_combo_clientela;
+     private List<String> lista_combo_look;
+     private List<String> lista_combo_tallas;
+     private List<String> lista_combo_material;
+     private List<String> lista_combo_color;
+     private List<String> lista_combo_coleccion;
 
+    public String getCabeceraocul() {
+        return cabeceraocul;
+    }
+
+    public void setCabeceraocul(String cabeceraocul) {
+        this.cabeceraocul = cabeceraocul;
+    }
+
+    public String getBotonocul() {
+        return botonocul;
+    }
+
+    public void setBotonocul(String botonocul) {
+        this.botonocul = botonocul;
+    }
+     
+     
+     
+
+    public List<Categoria> getLista_categoria() {
+        return lista_categoria;
+    }
+
+    public void setLista_categoria(List<Categoria> lista_categoria) {
+        this.lista_categoria = lista_categoria;
+    }
+
+    public List<Marcas> getLista_marca() {
+        return lista_marca;
+    }
+
+    public void setLista_marca(List<Marcas> lista_marca) {
+        this.lista_marca = lista_marca;
+    }
+
+    public List<Clientela> getLista_clientela() {
+        return lista_clientela;
+    }
+
+    public void setLista_clientela(List<Clientela> lista_clientela) {
+        this.lista_clientela = lista_clientela;
+    }
+
+    public List<Look> getLista_look() {
+        return lista_look;
+    }
+
+    public void setLista_look(List<Look> lista_look) {
+        this.lista_look = lista_look;
+    }
+
+    public List<Tallas> getLista_tallas() {
+        return lista_tallas;
+    }
+
+    public void setLista_tallas(List<Tallas> lista_tallas) {
+        this.lista_tallas = lista_tallas;
+    }
+
+    public List<Material> getLista_materiales() {
+        return lista_materiales;
+    }
+
+    public void setLista_materiales(List<Material> lista_materiales) {
+        this.lista_materiales = lista_materiales;
+    }
+
+    public List<Color> getLista_colores() {
+        return lista_colores;
+    }
+
+    public void setLista_colores(List<Color> lista_colores) {
+        this.lista_colores = lista_colores;
+    }
+
+    public List<Coleccion> getLista_colecciones() {
+        return lista_colecciones;
+    }
+
+    public void setLista_colecciones(List<Coleccion> lista_colecciones) {
+        this.lista_colecciones = lista_colecciones;
+    }
+
+    public List<String> getLista_combo_categorias() {
+        return lista_combo_categorias;
+    }
+
+    public void setLista_combo_categorias(List<String> lista_combo_categorias) {
+        this.lista_combo_categorias = lista_combo_categorias;
+    }
+
+    public List<String> getLista_combo_marcas() {
+        return lista_combo_marcas;
+    }
+
+    public void setLista_combo_marcas(List<String> lista_combo_marcas) {
+        this.lista_combo_marcas = lista_combo_marcas;
+    }
+
+    public List<String> getLista_combo_clientela() {
+        return lista_combo_clientela;
+    }
+
+    public void setLista_combo_clientela(List<String> lista_combo_clientela) {
+        this.lista_combo_clientela = lista_combo_clientela;
+    }
+
+    public List<String> getLista_combo_look() {
+        return lista_combo_look;
+    }
+
+    public void setLista_combo_look(List<String> lista_combo_look) {
+        this.lista_combo_look = lista_combo_look;
+    }
+
+    public List<String> getLista_combo_tallas() {
+        return lista_combo_tallas;
+    }
+
+    public void setLista_combo_tallas(List<String> lista_combo_tallas) {
+        this.lista_combo_tallas = lista_combo_tallas;
+    }
+
+    public List<String> getLista_combo_material() {
+        return lista_combo_material;
+    }
+
+    public void setLista_combo_material(List<String> lista_combo_material) {
+        this.lista_combo_material = lista_combo_material;
+    }
+
+    public List<String> getLista_combo_color() {
+        return lista_combo_color;
+    }
+
+    public void setLista_combo_color(List<String> lista_combo_color) {
+        this.lista_combo_color = lista_combo_color;
+    }
+
+    public List<String> getLista_combo_coleccion() {
+        return lista_combo_coleccion;
+    }
+
+    public void setLista_combo_coleccion(List<String> lista_combo_coleccion) {
+        this.lista_combo_coleccion = lista_combo_coleccion;
+    }
+     
+
+     
+     
+     
+     
+    public String getAccionocul() {
+        return accionocul;
+    }
+
+    public void setAccionocul(String accionocul) {
+        this.accionocul = accionocul;
+    }
     public String getFiltro() {
         return filtro;
     }
@@ -294,24 +476,69 @@ public class HomeRopa extends ActionSupport {
             return INPUT;
         }
         */
+        
+//             private List<String> lista_combo_categorias;
+//     private List<String> lista_combo_marcas;
+//     private List<String> lista_combo_clientela;
+//     private List<String> lista_combo_look;
+//     private List<String> lista_combo_tallas;
+//     private List<String> lista_combo_material;
+//     private List<String> lista_combo_color;
+//     private List<String> lista_combo_coleccion;
         switch(accion){
             case 'a': 
                 titulo = "Alta";
                 roId2 = 0;
-                clientela2 = null;
-                coleccion2 = null;
-                color2 = null;
-                look2 = null;
-                marcas2 = null;
-                tallas2 = null;
+                lista_marca = ControladoresDAO.cMarcas.RecuperaTodos("");
+                lista_combo_marcas = new ArrayList<String>();
+                for (Marcas Lm : lista_marca) {
+                    lista_combo_marcas.add(Lm.getMarcaNombre());
+                }
+                lista_clientela = ControladoresDAO.cClientela.RecuperaTodos("");
+                lista_combo_clientela = new ArrayList<String>();
+                for (Clientela Lcl : lista_clientela) {
+                    lista_combo_clientela.add(Lcl.getClientelaDescripcion());
+                }
                 roDescripcion2 = "";
+                lista_look = ControladoresDAO.cLook.RecuperaTodos("");
+                lista_combo_look = new ArrayList<String>();
+                for (Look Loo : lista_look) {
+                    lista_combo_look.add(Loo.getLookDescripcion());
+                }
+                lista_tallas = ControladoresDAO.cTallas.Recupera_Todos("");
+                lista_combo_tallas = new ArrayList<String>();
+                for (Tallas ta : lista_tallas) {
+                    lista_combo_tallas.add(ta.getTallaDescripcion());
+                }
                 roPrecio2 = 0.0;
                 roDescuento2 = 0.0;
+                lista_colores = ControladoresDAO.cColor.RecuperaTodos("");
+                lista_combo_color = new ArrayList<String>();
+                for (Color co : lista_colores) {
+                    lista_combo_color.add(co.getColorDescripcion());
+                }
+                lista_colecciones = ControladoresDAO.cColeccion.RecuperaTodos("");
+                lista_combo_coleccion = new ArrayList<String>();
+                for (Coleccion col : lista_colecciones) {
+                    lista_combo_coleccion.add(col.getColeccionDescripcion());
+                }               
                 roCaracteristicas2 = "";
                 roVisible2 = 0;
                 roUnidades2 = 0;
                 roFecha2 = null;
-                boton = "Alta";
+                lista_categoria = ControladoresDAO.cCategorias.RecuperaTodos("");
+                lista_combo_categorias = new ArrayList<String>();
+                for (Categoria Lc : lista_categoria) {
+                    lista_combo_categorias.add(Lc.getCatDescripcion());
+                }
+                lista_materiales = ControladoresDAO.cMaterial.Recupera_Todos("");
+                lista_combo_material = new ArrayList<String>();
+                for (Material mat : lista_materiales) {
+                    lista_combo_material.add(mat.getMaterialDescripcion());
+                }
+                            accionocul = "a";
+         cabeceraocul = "Alta";
+         botonocul = "Alta";
                 break;
             case 'm': 
                 titulo = "Modificación";
@@ -330,7 +557,15 @@ public class HomeRopa extends ActionSupport {
                 roVisible2 = 0;
                 roUnidades2 = 0;
                 roFecha2 = null;
-                boton = "Modificar";
+                for (Ropa Lr : lista_ropa) {
+                for(RopaCategoria rc: ropaCategorias2 ){
+                    lista_combo_categorias.add(Integer.toString(rc.getCategoria().getCatId()));
+                }
+                
+            }
+                                accionocul = "m";
+                cabeceraocul = "Modificar";
+                botonocul = "Modificar"; 
                 break;
             case 'e': 
                 titulo = "Eliminación";
@@ -349,7 +584,15 @@ public class HomeRopa extends ActionSupport {
                 roVisible2 = 0;
                 roUnidades2 = 0;
                 roFecha2 = null;
-                boton = "Eliminar";
+                for (Ropa Lr : lista_ropa) {
+                for(RopaCategoria rc: ropaCategorias2 ){
+                    lista_combo_categorias.add(Integer.toString(rc.getCategoria().getCatId()));
+                }
+                
+            }
+                accionocul = "e";
+                cabeceraocul = "Eliminar";
+                botonocul = "Eliminar";
                 break;
             default : 
                 titulo = "Error en titulo";
--- web/Vistas/pruebaCRUDformulario.jsp
+++ web/Vistas/pruebaCRUDformulario.jsp
@@ -17,30 +17,143 @@
         <s:form action="CrudActionRopa" theme="simple">
             <s:textfield type="hidden" name="accionocul"/>
             <table>
-
                 <tr>
                     <td>
-                        <s:label for="codigo">Codigo</s:label>  
+                        <s:label for="roId2">Codigo</s:label>  
                         </td>
                         <td>
-                        <s:textfield name="codigo2" readonly="true"></s:textfield>
+                        <s:textfield name="roId2" readonly="true"></s:textfield>
                         </td>
                     </tr>
                     <tr>
                         <td>
-                        <s:label for="nombre">Nombre</s:label>  
+                        <s:label for="clientela2">Clientela</s:label>  
                         </td>
+                        
                         <td>
-                        <s:textfield name="nombre2"/>
+                        <s:select list="lista_combo_clientela" label="lista_combo_clientela"/>
+
                     </td>
                 </tr>
                 <tr>
                     <td>
-                        <s:label for="IdRopa">IdRopa</s:label>  
+                        <s:label for="coleccion2">Coleccion</s:label>  
                         </td>
                         <td>
-                        <s:select list="lista" value="selection[%{#stat.index}]" label="lista" name="SeletRopa"/>
+                        <s:select list="lista_combo_coleccion" label="lista_combo_coleccion"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="color2">Color</s:label>  
+                        </td>
                         
+                        <td>
+                        <s:select list="lista_combo_color" label="lista_combo_color"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="look2">Look</s:label>  
+                        </td>
+                        
+                        <td>
+                        <s:select list="lista_combo_look" label="lista_combo_look"/>
+
+                    </td>
+                </tr>
+                <tr>
+                   
+                    <td>
+                        <s:label for="marcas2">Marcas</s:label>  
+                        </td>
+                        <td>
+                        <s:select list="lista_combo_marcas" label="lista_combo_marcas"/>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="tallas2">Tallas</s:label>  
+                        </td>
+                        
+                        <td>
+                        <s:select list="lista_combo_tallas" label="lista_combo_tallas"/>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roDescripcion2">Descripcion</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roDescripcion2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roPrecio2">Precio</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roPrecio2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roDescuento2">Descuento</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roDescuento2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roCaracteristicas2">Caracteristicas</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roCaracteristicas2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roVisible2">Visible</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roVisible2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roUnidades2">Unidades</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roUnidades2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="roFecha2">Fecha</s:label>  
+                        </td>
+                        <td>
+                        <s:textfield name="roFecha2"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="Categorias">Categorias</s:label>  
+                        </td>
+                        <td>
+                        <s:select list="lista_combo_categorias" label="lista_combo_categorias"/>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <s:label for="Material">Material</s:label>  
+                        </td>
+                        <td>
+                        <s:select list="lista_combo_material" label="lista_combo_material"/>
+
                     </td>
                 </tr>
                 <tr>
